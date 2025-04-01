84e31a8f4857065505d7c5be25fa21520d0e7701
duongnghoang <hoangduongatwork@gmail.com>
2025-04-01 21:50:42 +0700

    Dummy data

--- Data/PersonData.cs
+++ Data/PersonData.cs
@@ -7,12 +7,12 @@ public class PersonData
     public List<Person> Persons { get; } =
     [
         new("John", "Doe", Gender.Male, new DateTime(1990, 1, 1), "123-456-7890", "New York", true),
-        new("Jane", "Doe", Gender.Female, new DateTime(1992, 2, 2), "234-567-8901", "Los Angeles", false),
+        new("Jane", "Doe", Gender.Female, new DateTime(2000, 2, 2), "234-567-8901", "Los Angeles", false),
         new("Jim", "Beam", Gender.Male, new DateTime(1985, 3, 3), "345-678-9012", "Chicago", true),
-        new("Jack", "Daniels", Gender.Male, new DateTime(1988, 4, 4), "456-789-0123", "Houston", true),
+        new("Jack", "Daniels", Gender.Male, new DateTime(2000, 4, 4), "456-789-0123", "Houston", true),
         new("Jill", "Valentine", Gender.Female, new DateTime(1995, 5, 5), "567-890-1234", "Phoenix", false),
-        new("Chris", "Redfield", Gender.Male, new DateTime(1983, 6, 6), "678-901-2345", "Philadelphia", true),
+        new("Chris", "Redfield", Gender.Other, new DateTime(2022, 6, 6), "678-901-2345", "Philadelphia", true),
         new("Claire", "Redfield", Gender.Female, new DateTime(1991, 7, 7), "789-012-3456", "San Antonio", true),
-        new("Leon", "Kennedy", Gender.Male, new DateTime(1987, 8, 8), "890-123-4567", "San Diego", false)
+        new("Leon", "Kennedy", Gender.Male, new DateTime(2021, 8, 8), "890-123-4567", "San Diego", false)
     ];
 }
